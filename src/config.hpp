56eaa5e656fd897390322e14664d79bf07df5d20
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-11-13 03:05:59 +0100

    production server IP

--- src/config.hpp
+++ src/config.hpp
@@ -13,7 +13,8 @@ const char* app_version = "0.1.3";
 const char* default_currency_pair = "BTCUSD";
 const char* update_server_url = "10.0.1.48:4567";
 
+const char* ticker_server_ip = "139.59.138.189";
 //const char* ticker_server_ip = "178.217.242.3";
-const char* ticker_server_ip = "10.0.1.48";
+//const char* ticker_server_ip = "10.0.1.48";
 const int ticker_server_port = 8081;
 const char* ticker_server_url = "/";
