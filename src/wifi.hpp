30641b58e45c67f1b316bd6e7259869d2f85f6fb
Jiri Kubicek <jiri.kubicek@kraxnet.cz>
2017-11-14 02:48:31 +0100

    nastavitelny ticker server

--- src/wifi.hpp
+++ src/wifi.hpp
@@ -4,17 +4,20 @@
 
 extern const char* default_currency_pair;
 extern const char* update_server_url;
+extern const char* ticker_server_ip;
 
 class WiFiCore
 {
 public:
   WiFiCore(Display &display, AP_list *ap_list) : m_display(display), m_ap_list(ap_list), m_wifimanager(WiFiManager()),
     m_currency_pair(new WiFiManagerParameter("pair", "Currency pair", default_currency_pair, 10)),
+    m_ticker_server(new WiFiManagerParameter("ticker_server", "Ticker server", ticker_server_ip, 20)),
     m_update_server(new WiFiManagerParameter("update_server", "Update server", update_server_url, 20))
   {
     // TODO: read/save custom data on eeprom
 
     m_wifimanager.addParameter(m_currency_pair);
+    m_wifimanager.addParameter(m_ticker_server);
     m_wifimanager.addParameter(m_update_server);
 
     m_wifimanager.setSaveConfigCallback(&saveCallback);
@@ -38,4 +41,5 @@ private:
   AP_list *m_ap_list;
   WiFiManagerParameter *m_currency_pair;
   WiFiManagerParameter *m_update_server;
+  WiFiManagerParameter *m_ticker_server;
 };
