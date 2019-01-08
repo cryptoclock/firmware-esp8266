1f96fc7e694c50d689ed1c55d7c8ded3eb932dfe
Jan Dvorak <snoutmate@gmail.com>
2019-01-08 05:04:15 +0100

    7segment preliminary work

--- src/display_neopixel.hpp
+++ src/display_neopixel.hpp
@@ -25,27 +25,16 @@
 #include "config_common.hpp"
 #define FASTLED_INTERNAL
 #define FASTLED_ESP8266_NODEMCU_PIN_ORDER
+#define FASTLED_ALLOW_INTERRUPTS 0
 #include "FastLED.h"
-// #define NUM_LEDS 60
-// CRGB leds[NUM_LEDS];
-// void setup() { FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS); }
-// void loop() {
-// 	leds[0] = CRGB::White; FastLED.show(); delay(30);
-// 	leds[0] = CRGB::Black; FastLED.show(); delay(30);
-// }
 #include "display.hpp"
 
 namespace Display {
 class Neopixel : public DisplayT
 {
 public:
-  Neopixel(const int milis_per_tick, const int width, const int height) :
-    DisplayT(milis_per_tick), m_width(width), m_height(height),
-    m_leds(new CRGB[width*height])
-  {
-//    m_display->begin();
-//    m_display->max_power(5,450); // 5V, 450mA
-  }
+  Neopixel(const int milis_per_tick, const int width, const int height);
+
   template<uint8_t DataPin>
   void initialize()
   {
@@ -53,7 +42,7 @@ public:
   }
 
   void displayText(const String& value, const Coords& coords);
-  void displayNumber(const int number, const int length, const int position, const bool zero_fill) {}
+  void displayNumber(const int number, const int length, const int position, const bool zero_fill);
   void displayBitmap(const unsigned char *bitmap, const Coords& coords, const int w, const int h) {}
   void displayBitmapP(const unsigned char *bitmap, const Coords& coords, const int w, const int h) {}
 
@@ -72,8 +61,8 @@ public:
   int getDisplayWidth() { return m_width; }
   int getDisplayHeight() { return m_height; }
   int getCurrentFontHeight() { return 1; }
-  bool isNumeric(void) { return false; }
-  bool isGraphic(void) { return true; }
+  bool isNumeric(void) { return true; }
+  bool isGraphic(void) { return false; }
 
 private:
   const int m_width;
