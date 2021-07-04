6ac2b7a03f45642c788863ef8a4f0cee347131a5
Jan Dvořák <jan.dvorak@kraxnet.cz>
2021-07-04 20:20:11 +0200

    Updated trackdecody.py with proper paths

--- tools/trackdecode.py
+++ tools/trackdecode.py
@@ -3,14 +3,16 @@
 import subprocess
 import os
 
-print os.getcwd()
-
-xtensa_dir = ".platformio/packages/toolchain-xtensa/bin/"
+from pathlib import Path
+home_dir = str(Path.home())
+xtensa_dir = home_dir + "/" + ".platformio/packages/toolchain-xtensa/bin/"
 cwd = "tools/"
+
+build_dir = ".pio/build/3DA0100/"
 
 # https://github.com/littleyoda/EspStackTraceDecoder
 decoder = cwd + "EspStackTraceDecoder.jar"
-firmware_elf = cwd + "firmware.elf"
+firmware_elf = build_dir + "firmware.elf"
 trace_txt = cwd + "trace.txt"
 addr2line = xtensa_dir + "xtensa-lx106-elf-addr2line"
 
